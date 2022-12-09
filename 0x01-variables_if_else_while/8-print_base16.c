662b6c505d1d95ecd52cff17c9055cc450139766
kevinngugi3576 <kevinngugi197@gmail.com>
2022-12-09 14:43:09 -0800

    lowecase

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -2,6 +2,7 @@
 /**
  * main - Entry point
  *
+ * Return: Always 0 (success)
  */
 
 int main(void)
@@ -11,6 +12,7 @@ int main(void)
 	int d;
 
 	c = 'a';
+	d = 0;
 	while
 		(d < 10) {
 			putchar(d + '0');
