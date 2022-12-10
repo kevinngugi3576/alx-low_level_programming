a94d49a2563f3c3d9ad76e6c2010082cad5a3719
kevinngugi3576 <kevinngugi197@gmail.com>
2022-12-09 17:05:05 -0800

    combinations

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -4,16 +4,18 @@
  *
  * Return:	Always	0	(success)
  */
+
 int	main(void)
 {
 	char c;
-
+	
 	c = 'a';
 	while
-		(c <= 'z')	{
-				putchar(c);
-				c++;
-			}
-		putchar('\n');
-		return	(0);
-}		
+		(c <= 'z') {
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return	(0);
+}
+
