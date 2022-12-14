99925c19ff0fca5d1e94d0e9da1d0259745117c2
kevinngugi3576 <kevinngugi197@gmail.com>
2022-12-14 14:56:56 -0800

    final task 1

--- 0x02-functions_nested_loops/1-main.c
+++ 0x02-functions_nested_loops/1-main.c
@@ -2,16 +2,14 @@
 
 /**
  *
- * main - check the code for Holberton School students.
+ * main - check the code
  *
  *Return: Always 0.
  *
  */
 
 int main(void)
-
 {
-	
+	void print_alphabet(void);
 	return (0);
-
 }
