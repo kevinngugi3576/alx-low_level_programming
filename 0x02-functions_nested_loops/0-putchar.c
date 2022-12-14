45596a53ba77b325db2a1eb43609a79c0514db94
kevinngugi3576 <kevinngugi197@gmail.com>
2022-12-14 07:19:50 -0800

    putchar task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,6 @@
-#include <main.h>
+#include "main.h"
 #include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - prints _putchar and a new line
---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _putchar - writes c to stdout
+ * c: character tp print
+ *
+ * Return: on success 1
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+
+}
