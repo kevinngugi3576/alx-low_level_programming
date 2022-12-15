0ec9b4211b7edda24a52c57748339625847e69e6
kevinngugi3576 <kevinngugi197@gmail.com>
2022-12-14 16:16:18 -0800

    dvance 13

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints sum of all multiples of 3,5 upto 1024
+ * Rreturn: Always (success)
+ */
+
+int main(void)
+{
+	int i, z - 0;
+
+	while (i < 1024)
+	{
+		if ((i % 3 == 0) || (i % 5 ==))
+		{
+			z += i;
+		}
+		i++;
+	}
+	printf(("%d\n", z);
+			return (0):
+}
