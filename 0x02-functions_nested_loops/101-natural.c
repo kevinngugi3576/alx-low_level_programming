b0ae442d58dab59217cd5eb5fb3a5805bf59ebeb
kevinngugi3576 <kevinngugi197@gmail.com>
2022-12-14 16:26:15 -0800

    correction Task 13

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -7,16 +7,16 @@
 
 int main(void)
 {
-	int i, z - 0;
+	char  i, z = 0;
 
 	while (i < 1024)
 	{
-		if ((i % 3 == 0) || (i % 5 ==))
+		if ((i % 3 == 0) || (i % 5 == 0))
 		{
 			z += i;
 		}
 		i++;
 	}
-	printf(("%d\n", z);
-			return (0):
+	printf("%d\n", z);
+			return (0);
 }
