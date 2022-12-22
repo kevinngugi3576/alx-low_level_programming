12e698b42d2b34b48adc1c4e6eb6638728d41775
kevinngugi3576 <kevinngugi197@gmail.com>
2022-12-22 12:31:25 -0800

    Write a function that adds two numbers.

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
-char *add_strings(char *n1, char *n2, char*r, int r_index);
-char *infinite_add(char *n1, chr *n2, char *r, int size_r);
+char *add_strings(char *n1, char *n2, char *r, int r_index);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
 
 /**
  * add_strings - adds the numbers stored in two strings
@@ -29,14 +29,14 @@ char *add_strings(char *n1, char *n2, char *r, int r_index)
 	{
 		num = (*n1 - '0') + tens;
 		*(r + r_index) = (num % 10) + '0';
-		tens =num / 10;
+		tens = num / 10;
 	}
 
 	for (; *n2; n2--, r_index--)
 	{
 		num = (*n2 - '0') + tens;
 		*(r + r_index) = (num % 10) + '0';
-		tens =num / 10;
+		tens = num / 10;
 	}
 
 	if (tens && r_index >= 0)
@@ -45,13 +45,13 @@ char *add_strings(char *n1, char *n2, char *r, int r_index)
 		return (r + r_index);
 	}
 
-	else if (tens && r_index < 0 )
+	else if (tens && r_index < 0)
 		return (0);
 
 	return (r + r_index + 1);
 }
 /**
- * infinite_add: adds two numbers
+ * infinite_add - adds two numbers
  * @n1: first number to be added
  * @n2: second number to be added
  * @r: buffer to strore result
@@ -60,14 +60,14 @@ char *add_strings(char *n1, char *n2, char *r, int r_index)
  * Return: if r can store the sum - a pointer to the result
  * if r cannot store the sum - 0
  */
-char *infinite_add(char *n1, char *n2, char*r, int size_r)
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
 	int index, n1_len = 0, n2_len = 0;
 
-	for (index = =; *(n1 * index); index++)
+	for (index = 0; *(n1 + index); index++)
 		n1_len++;
 
-	for (index = =; *(n2 * index); index++)
+	for (index = 0; *(n2 + index); index++)
 		n2_len++;
 
 	if (size_r <= n1_len + 1 || size_r <= n2_len + 1)
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -23,4 +23,8 @@ void print_number(int n);
 
 int _putchar(char c);
 
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+
+int main(void);
+
 #endif
