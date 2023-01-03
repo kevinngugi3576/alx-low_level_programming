8886bec12e0e76d016b164ae4259bae56387a945
kevinngugi3576 <kevinngugi197@gmail.com>
2023-01-03 11:49:42 -0800

    Task 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -12,11 +12,11 @@ char *_strchr(char *s, char c)
 {
 	int index;
 
-	for (index = 0; S[index] >= '\0'; index)
+	for (index = 0; s[index] >= '\0'; index++)
 	{
 		if (s[index] == c)
 			return (s + index);
 	}
 
-	return ('\0');
+	return (NULL);
 }
