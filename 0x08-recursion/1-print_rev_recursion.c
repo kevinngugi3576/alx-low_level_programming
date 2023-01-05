e71ddaf9cd779c95d87d7d250b5d7509aeb00789
kevinngugi3576 <kevinngugi197@gmail.com>
2023-01-05 07:55:54 -0800

    Task 2

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -13,5 +13,7 @@ void _print_rev_recursion(char *s)
 		return;
 	}
 
-	_print_rev_recursion(s + 10);
+	_print_rev_recursion(s + 1);
+
+	putchar(*s);
 }
--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -8,15 +8,14 @@
  * Return: always 0 (success)
  */
 
-int_strlen_recursion(char *s)
+int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		return 0;
+		return (0);
 	}
-
-	else 
+	else
 	{
-		return 1 + _strlen_recursion(s + 1);
+		return (1 + _strlen_recursion(s + 1));
 	}
 }
