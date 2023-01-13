bf4e5e03748d93c858d465a2f172903ea6e3bcb5
kevinngugi3576 <kevinngugi197@gmail.com>
2023-01-13 04:48:58 -0800

    Task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -34,7 +34,7 @@ char *string_ncontact(char *s1, char *s2, unsigned int n)
 
 	if (n > size2)
 		n = size2;
-	p = malloc((size1 + n + 1) * sizeos(char));
+	p = malloc((size1 + n + 1) * sizeof(char));
 
 	if (p == NULL)
 		return (0);
