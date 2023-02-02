a244dbcc80a20d12bdb2f4311a328bf5ab1fdf33
kevinngugi3576 <kevinngugi197@gmail.com>
2023-02-02 05:18:24 -0800

    safe version

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,18 +1,18 @@
 #ifndef LISTS_H
 #define LISTS_H
-
 #include <stdlib.h>
-/**
- * struct listint_t - singly linked list
- * @n: integer
- * @next: points to the next node
- */
 
+/**
+ * struct listint_t - singly linked lists
+ * @n: integer
+ * @next: points to the next node 
+ */
 typedef struct listint_t
 {
 	int n;
 	struct listint_t *next;
 } listint_t;
+
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
@@ -28,5 +28,4 @@ listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
-
 #endif
